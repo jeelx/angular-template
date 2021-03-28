42b882d86259ef8e675c1ba6a56831c6aff94812
Jeelx <julian@schultehullern.de>
2021-03-28 10:31:02 +0200

    update prettier

--- src/test.ts
+++ src/test.ts
@@ -1,23 +1,23 @@
-import 'jest-preset-angular/setup-jest'
+import 'jest-preset-angular/setup-jest';
 
-Object.defineProperty(window, 'CSS', { value: null })
+Object.defineProperty(window, 'CSS', { value: null });
 Object.defineProperty(window, 'getComputedStyle', {
-    value: () => {
-        return {
-            display: 'none',
-            appearance: ['-webkit-appearance'],
-        }
-    },
-})
+  value: () => {
+    return {
+      display: 'none',
+      appearance: ['-webkit-appearance'],
+    };
+  },
+});
 
 Object.defineProperty(document, 'doctype', {
-    value: '<!DOCTYPE html>',
-})
+  value: '<!DOCTYPE html>',
+});
 Object.defineProperty(document.body.style, 'transform', {
-    value: () => {
-        return {
-            enumerable: true,
-            configurable: true,
-        }
-    },
-})
+  value: () => {
+    return {
+      enumerable: true,
+      configurable: true,
+    };
+  },
+});
